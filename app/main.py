2d671c836e32521df3600b04648408ef8f10569b
maldonadomatias <molporron@gmail.com>
2023-09-04 14:27:56 -0300

    first commit

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.api.api import api_router
 
+
 app = FastAPI(title="Transfermarkt API")
 app.include_router(api_router)
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can restrict this to specific origins in a production environment
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", include_in_schema=False)
